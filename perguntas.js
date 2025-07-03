61044b16d1f7877b9549911e1b69bdadcbdcf4f9
helena-05 <161738204+helena-05@users.noreply.github.com>
2025-07-03 07:45:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Paises',
+    'qual e o menor pais do mundo? ',
+    'O vaticano'
+)
+
+criaCartao(
+    'historia',
+    'Quando começou a primeira guerra mundial?',
+    '28 de julho de 1914'
+)
+
+criaCartao(
+    'Esportes',
+    'qual e a maior altura de uma rede de volei?',
+    '2,43 metros para jogos masculinos e 2,24 para jogos femininos'
+)
+
+criaCartao(
+    'cinema',
+    'qual e o filme mais assistido do mundo?',
+    'avatar'
+)
